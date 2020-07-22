c1cc2d28ba4aeb4fe9ba99b1c5d864a1b01063e0
supernick125 <nickhealy33@gmail.com>
2020-07-21 20:22:21 -0400

    Frontend form merge

--- react-postgres/src/App.js
+++ react-postgres/src/App.js
@@ -1,59 +1,60 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
+import DescriptionComp from './DescriptionComp';
+import AlumniForm from './AlumniForm';
 
-function App() {
-  const [students, setStudents] = useState(false);
-  useEffect(() => {
-    getStudent();
-  }, []);
-  function getStudent() {
-    fetch("http://localhost:3001")
-    .then(response => {
-      return response.text();
-    })
-    .then(data => {
-      setStudents(data);
-    });
+class App extends React.Component {
+  render() {
+    return (
+        <div class="container-fluid h-100">
+          <div class="row h-100">
+            <div id="description" class="col-md-7 p-3">
+              <DescriptionComp />
+            </div>
+            <div class="col-md-5 columbiaBlue p-3">
+              <AlumniForm />
+            </div>
+          </div>
+        </div>
+    );
   }
-  function createStudent() {
-    let name = prompt("Enter student name");
-    let email = prompt("Enter student email");
-    fetch("http://localhost:3001/students", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({name, email}),
-    })
-    .then(response => {
-      return response.text();
-    })
-    .then(data => {
-      alert(data);
-      getStudent();
-    });
-  }
-  function deleteStudent() {
-    let id = prompt("Enter student id");
-    fetch(`http://localhost:3001/students/${id}`, {
-      method: "DELETE",
-    })
-    .then(response => {
-      return response.text();
-    })
-    .then(data => {
-      alert(data);
-      getStudent();
-    });
-  }
-  return (
-    <div>
-      {students ? students: "There is no student data available"}
-      <br />
-      <button onClick={createStudent}>Add student</button>
-      <br />
-      <button onClick={deleteStudent}>Delete student</button>
-    </div>
-  );
 }
 
 export default App;
+
+// function App() {
+//   const [students, setStudents] = useState(false);
+//   useEffect(() => {
+//     getStudent();
+//   }, []);
+//   function getStudent() {
+//     fetch("http://localhost:3001")
+//     .then(response => {
+//       return response.text();
+//     })
+//     .then(data => {
+//       setStudents(data);
+//     });
+//   }
+//   function deleteStudent() {
+//     let id = prompt("Enter student id");
+//     fetch(`http://localhost:3001/students/${id}`, {
+//       method: "DELETE",
+//     })
+//     .then(response => {
+//       return response.text();
+//     })
+//     .then(data => {
+//       alert(data);
+//       getStudent();
+//     });
+//   }
+//   return (
+//     <div>
+//       {students ? students: "There is no student data available"}
+//       <br />
+//       <button onClick={createStudent}>Add student</button>
+//       <br />
+//       <button onClick={deleteStudent}>Delete student</button>
+//     </div>
+//   );
+// }
