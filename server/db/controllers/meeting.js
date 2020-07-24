41b1e63de0155ff7be8fb32ef6657f96d3f1bed3
supernick125 <nickhealy33@gmail.com>
2020-07-23 22:52:58 -0400

    Rename client folder

---
+++ server/db/controllers/meeting.js
@@ -0,0 +1,15 @@
+const pool = require('../index');
+
+const getMeetings = async (req, res) => {
+  try {
+    const meetings = await pool.query(
+      'SELECT * FROM MEETINGS ORDER BY meeting_id ASC'
+    )
+  } catch (error) {
+    return res.status(500).json({ message: 'There was an error while searching. Please try again later.'})
+  }
+}
+
+module.exports = {
+  getMeetings
+}
