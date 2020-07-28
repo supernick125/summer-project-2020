5b89ace1370c52c9b7bf77d84d9f74fc5f6ffbea
supernick125 <nickhealy33@gmail.com>
2020-07-28 14:34:46 -0400

    Incorporate Gabe's link functions

--- client/src/AlumniForm.js
+++ client/src/AlumniForm.js
@@ -4,60 +4,118 @@ class AlumniForm extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      firstName: "",
-      lastName: "",
-      email: "",
-      link: "",
+      firstName: '',
+      lastName: '',
+      email: '',
+      username: '',
+      password: ''
     };
   }
 
-  createStudent = () => {
-    fetch("http://localhost:3001/students", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({this.state.firstName, this.state.lastName, this.state.email, this.state.link}),
-    })
-    .then(response => {
-      return response.text();
-    })
-    .then(data => {
-      alert(data);
-      getStudent();
-    });
-  }
+  // getUsers = () => {
+  //   fetch('http://localhost:3001/api/user')
+  //   .then(response => {
+  //     return response.text();
+  //   })
+  //   .then(data => {
+  //     alert(data);
+  //   });
+  // }
+  //
+  // registerUser = async () => {
+  //   const resp = await fetch(`${BASE_URL}/api/user/register`, {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify(this.state)
+  //   });
+  //
+  //
+  //   fetch('http://localhost:3001/api/user/register', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify({this.state.firstName, this.state.lastName, this.state.email, this.state.link}),
+  //   })
+  //   .then(response => {
+  //     return response.text();
+  //   })
+  //   .then(data => {
+  //     alert(data);
+  //     getStudent();
+  //   });
+  // }
+  //
+  // deleteUser = () => {
+  //   fetch(`http://localhost:3001/api/user/${id}`, {
+  //     method: 'DELETE',
+  //   })
+  //   .then(response => {
+  //     return response.text();
+  //   });
+  // }
 
+  //Handle input change
   handleChange = (event) => {
     this.setState({[event.target.name]: event.target.value});
   }
 
+  //Handle form submit
   handleSubmit = (event) => {
-    alert(`Name is: ${this.state.firstName} ${this.state.lastName} Email is: ${this.state.email} Link is: ${this.state.link}`);
+    console.log(`Name: ${this.state.firstName} ${this.state.lastName} Email: ${this.state.email} Username: ${this.state.username} Password: ${this.state.password}`);
     event.preventDefault();
+
+    const data = this.state;
+    fetch('/api/user/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(response => {
+      return response.text();
+    })
+    .catch(error => {
+      console.log(error);
+    });
+
+    this.setState({
+      firstName: '',
+      lastName: '',
+      email: '',
+      username: '',
+      password: ''
+    });
   }
 
   render() {
     return (
-      <form action="" class="m-4" onSubmit={this.handleSubmit}>
-        <div class="form-group">
-          <label for="firstName">First Name:</label>
-          <input type="text" class="form-control" id="firstName" name="firstName" value={this.state.firstName} onChange={this.handleChange}/>
+      <form action='' class='m-4' onSubmit={this.handleSubmit}>
+        <div class='form-group'>
+          <label for='firstName'>First Name:</label>
+          <input type='text' class='form-control' id='firstName' name='firstName' value={this.state.firstName} onChange={this.handleChange}/>
         </div>
-        <div class="form-group">
-          <label for="lastName">Last Name:</label>
-          <input type="text" class="form-control" id="lastName" name="lastName" value={this.state.lastName} onChange={this.handleChange}/>
+        <div class='form-group'>
+          <label for='lastName'>Last Name:</label>
+          <input type='text' class='form-control' id='lastName' name='lastName' value={this.state.lastName} onChange={this.handleChange}/>
         </div>
-        <div class="form-group">
-          <label for="email">Email Address:</label>
-          <input type="text" class="form-control" id="email" aria-describedby="emailHelp" name="email" value={this.state.email} onChange={this.handleChange}/>
-          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        <div class='form-group'>
+          <label for='email'>Email Address:</label>
+          <input type='text' class='form-control' id='email' aria-describedby='emailHelp' name='email' value={this.state.email} onChange={this.handleChange}/>
+          <small id='emailHelp' class='form-text text-muted'>We'll never share your email with anyone else.</small>
         </div>
-        <div class="form-group">
-          <label for="bio">Website / Link / Curriculum Vitae:</label>
-          <input type="text" class="form-control" id="bio" name="link" value={this.state.link} onChange={this.handleChange}/>
+        <div class='form-group'>
+          <label for='bio'>Username:</label>
+          <input type='text' class='form-control' id='username' name='username' value={this.state.username} onChange={this.handleChange}/>
         </div>
-        <button type="submit" class="btn btn-primary countryBlue">Submit</button>
+        <div class='form-group'>
+          <label for='bio'>Password:</label>
+          <input type='text' class='form-control' id='password' name='password' value={this.state.password} onChange={this.handleChange}/>
+        </div>
+        <button type='submit' class='btn btn-primary countryBlue'>Submit</button>
       </form>
     );
   }
