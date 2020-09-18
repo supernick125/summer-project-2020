5c7f82df0a7dd65d302964e435e0766ff6a3588b
Zhihan Chen <zhihanc@MacBook-Pro.fios-router.home>
2020-09-18 17:01:23 -0400

    Made the RegisterNavbar component

---
+++ client/src/components/RegisterNavbar/RegisterNavbar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Container, Nav, Navbar, Form, FormControl, InputGroup, Button } from 'react-bootstrap';
+import './style.css';
+
+export default () => {
+  return (
+
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Alumni & Students</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Sign Up</Nav.Link>
+            <Nav.Link href="#link">About Us</Nav.Link>
+          </Nav>
+          <Form inline>
+          <Form.Label htmlFor="loginEmail" srOnly>
+            Email
+          </Form.Label>
+          <InputGroup className="mr-sm-2">
+            <InputGroup.Prepend>
+              <InputGroup.Text>School Email</InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl 
+              id="loginEmail"
+              placeholder="" />
+            </InputGroup>
+            <Form.Label htmlFor="loginPassword" srOnly>
+              Password
+            </Form.Label>
+            <InputGroup className="mr-sm-2">
+              <InputGroup.Prepend>
+                <InputGroup.Text>Password</InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl 
+                id="loginPassword" 
+                placeholder="" />
+            </InputGroup>
+            <Button variant="outline-primary">Login</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+  );
+}
