aeb16f53beae0b5852bceaa78e154f93b801a163
gklass19 <41346742+gklass19@users.noreply.github.com>
2020-09-19 20:17:15 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,35 @@ import RegisterPage from './pages/Register/RegisterPage';
 import HomePage from './pages/Home/HomePage';
 import MeetingsPage from './pages/Meetings/MeetingsPage'
 
+function logInCheck()
+{
+  return(false)
+}
+
 export default (props) => {
-  return (
-    <div className='h-100'>
+  if(logInCheck()==false)
+  {
+    return (
+      <div className='h-100'>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              {/*<Redirect to="/home" />*/}
+              <RegisterPage />
+            </Route>
+            <Route path="/home">
+              <HomePage />
+            </Route>
+            <Route path='*'>
+              <h1>Not Found</h1>
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    );
+  }else{
+    return (
+      <div className='h-100'>
       <Router>
         <Switch>
           <Route exact path="/">
@@ -26,5 +52,6 @@ export default (props) => {
         </Switch>
       </Router>
     </div>
-  );
+    );
+  }
 }
