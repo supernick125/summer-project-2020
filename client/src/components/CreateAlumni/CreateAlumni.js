1b0731e8a20e36d9ac03ff290db0489d292ffa89
supernick125 <nickhealy33@gmail.com>
2020-09-25 22:25:31 -0400

    Small bug fixes

--- client/src/components/CreateAlumni/CreateAlumni.js
+++ client/src/components/CreateAlumni/CreateAlumni.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
-import Axios from 'axios'
-import { Container, Form, Button } from 'react-bootstrap'
-import './style.css'
+import React, { useState } from 'react';
+import Axios from 'axios';
+import { Container, Form, Button } from 'react-bootstrap';
+import './style.css';
 
 export default () => {
 
@@ -14,71 +14,62 @@ export default () => {
     password: ''
   });
 
-  const registerUser = () => {
-    Axios.post('http://localhost:3001/api/user/register/alumni', user)
-      .then(response => {
-        console.log(response);
-        console.log(response.data);
-      })
-      .catch(error => {
-        console.error(error);
+  const updateUser = (event) => {
+    setUser({ ...user, [event.target.name]: event.target.value });
+  }
+
+  const registerUser = async (event) => {
+    event.preventDefault();
+    try {
+      const resp = await Axios({
+        method: 'POST',
+        url: '/api/auth/register',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        data: user
       });
       setUser({
-        userType: '1',
+        userType: '2',
         firstName: '',
         lastName: '',
         graduationYear: '',
         email: '',
         password: ''
-      });
-  }
-
-  //Handle input change
-  const handleChange = (event) => {
-    setUser({ ...user, [event.target.name]: event.target.value });
-  }
-
-  //Handle form submit
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(`Name: ${user.firstName} ${user.lastName} Email: ${user.email} Password: ${user.password}`);
-    //Register new user
-    registerUser();
+      })
+    }catch(error) {
+      console.error('Error creating user');
+    }
   }
 
   return (
     <Container>
       <h1>Create an Alumni Account</h1>
-
-      <Form className='form' onSubmit={handleSubmit}>
+      <Form className='form' onSubmit={registerUser}>
         <Form.Group>
           <Form.Label>First Name:</Form.Label>
-          <Form.Control type="text" name="firstName" value={user.firstName} onChange={handleChange}/>
+          <Form.Control type="text" name="firstName" value={user.firstName} onChange={updateUser}/>
         </Form.Group>
         <Form.Group>
           <Form.Label>Last Name:</Form.Label>
-          <Form.Control type="text" name="lastName" value={user.lastName} onChange={handleChange}/>
+          <Form.Control type="text" name="lastName" value={user.lastName} onChange={updateUser}/>
         </Form.Group>
         <Form.Group>
           <Form.Label>Graduation Year:</Form.Label>
-          <Form.Control type="text" name="graduationYear" value={user.graduationYear} onChange={handleChange}/>
+          <Form.Control type="text" name="graduationYear" value={user.graduationYear} onChange={updateUser}/>
         </Form.Group>
         <Form.Group controlId="formBasicEmail">
           <Form.Label>School Email Address:</Form.Label>
-          <Form.Control type="email" name="email" value={user.email} onChange={handleChange}/>
+          <Form.Control type="email" name="email" value={user.email} onChange={updateUser}/>
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
         </Form.Group>
         <Form.Group>
           <Form.Label>Password:</Form.Label>
-          <Form.Control type="text" name="password" value={user.password} onChange={handleChange}/>
+          <Form.Control type="text" name="password" value={user.password} onChange={updateUser}/>
         </Form.Group>
-        <Form.Group>
-        <Form.Label>Re-enter Password:</Form.Label>
-        <Form.Control type="text" name="passwordCheck" value={user.passwordCheck} onChange={handleChange}/>
-      </Form.Group>
-        <Button variant="primary" type="submit" value="Submit" onClick={handleSubmit}>Submit</Button>
+        <Button variant="primary" type="submit">Submit</Button>
       </Form>
     </Container>
   );
--- client/src/components/CreateMeeting/CreateMeeting.js
+++ client/src/components/CreateMeeting/CreateMeeting.js
@@ -1,94 +1,52 @@
-import React, { useState } from 'react'
-import Axios from 'axios'
-import { Container, Form, Button } from 'react-bootstrap'
-import './style.css'
+import React, { useState } from 'react';
+import Axios from 'axios';
+import { Container, Form, Button } from 'react-bootstrap';
+import './style.css';
 
 export default () => {
 
   const [meeting, setMeeting] = useState({
-    userType: '2',
-    firstName: '',
-    lastName: '',
-    graduationYear: '',
-    email: '',
-    password: '',
-    passwordCheck: '',
-    whichRegister: true,
     host: '',
-    start: '',
-    virtual: ''
+    start: ''
   });
 
-  //host_first, host_last, email, start, (virtual)
-
-  // const emailCheck = function() {
-  //   return(user.email.includes('columbia.edu') || user.email.includes('barnard.edu'))
-  // }
-
-  const passwordCheck = function () {
-    return(user.password === user.passwordCheck)
+  const updateMeeting = (event) => {
+    setMeeting({ ...meeting, [event.target.name]: event.target.value });
   }
 
-  const createMeeting = () => {
-    Axios.post('http://localhost:3001/api/meeting/register', user)
-      .then(response => {
-        console.log(response);
-        console.log(response.data);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-
-      // reset state
-      this.setState({
-        host: '',
-        start: '',
-        virtual: true
-      })
-  }
-
-
-  //Handle input change
-  const handleChange = (event) => {
-    setUser({ ...user, [event.target.name]: event.target.value });
-  }
-
-  //Handle form submit
-  const handleSubmit = (event) => {
-    // WITH emailCheck()
-    // if (emailCheck() && passwordCheck()) {
-    //   console.log(`Name: ${user.firstName} ${user.lastName} Email: ${user.email} Password: ${user.password}`);
-    //   //Register new user
-    //   registerUser();
-    // } else if (!emailCheck()) {
-    //   alert('Please use your columbia.edu or barnard.edu email!')
-    // } else {
-    //   alert('Please re-enter the same password!')
-    // }
-    //WITHOUT emailCheck()
-    console.log(`Host id: ${user.host} Start: ${user.start} Virtual: ${user.virutal}`);
-    createMeeting();
+  const createMeeting = async (event) => {
     event.preventDefault();
+    try {
+      const resp = await Axios({
+        method: 'POST',
+        url: '/api/meeting',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        data: meeting
+      });
+      setMeeting({
+        host: '',
+        start: ''
+      })
+    }catch(error) {
+      console.error('Error creating meeting');
+    }
   }
 
   return (
     <Container>
       <h1>Create a Meeting</h1>
-
-      <Form className='form'>
+      <Form className='form' onSubmit={createMeeting}>
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Host ID (Alumni Name)</Form.Label>
-          <Form.Control type="text" name="host" value={user.host} onChange={handleChange}/>
+          <Form.Control type="text" name="host" value={meeting.host} onChange={updateMeeting}/>
         </Form.Group>
-
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Start (Start Time)</Form.Label>
-          <Form.Control type="text" name="start" value={user.start} onChange={handleChange}/>
+          <Form.Control type="text" name="start" value={meeting.start} onChange={updateMeeting}/>
         </Form.Group>
-        <Form.Group controlId="formBasicCheckbox">
-          <Form.Check type="text" name="virtual" value={user.virtual} onChange={handleChange}/>
-        </Form.Group>
-        <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
+        <Button variant="primary" type="submit">Submit</Button>
       </Form>
     </Container>
   );
