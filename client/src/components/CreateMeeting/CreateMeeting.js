3de7ca76c60b87a38a2dbae86340acb60870d523
Zhihan Chen <zhihanc@MacBook-Pro.fios-router.home>
2020-10-19 12:15:47 -0400

    Unlimited char input for alumni bios on admin page

--- client/src/components/CreateMeeting/CreateMeeting.js
+++ client/src/components/CreateMeeting/CreateMeeting.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Axios from 'axios';
-import { Container, Form, Button } from 'react-bootstrap';
+import { Container, Form, Button, FormGroup } from 'react-bootstrap';
 import './style.css';
 
 export default () => {
@@ -40,12 +40,32 @@ export default () => {
       <Form className='form' onSubmit={createMeeting}>
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Host ID (Alumni Name)</Form.Label>
-          <Form.Control type="text" name="hostid" value={meeting.hostid} onChange={updateMeeting} required/>
+          <Form.Control 
+            type="text" 
+            name="hostid" 
+            value={meeting.hostid} 
+            onChange={updateMeeting} 
+            required/>
         </Form.Group>
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Start (Start Time)</Form.Label>
-          <Form.Control type="text" name="starttime" value={meeting.starttime} onChange={updateMeeting} required/>
+          <Form.Control 
+            type="text" 
+            name="starttime" 
+            value={meeting.starttime} 
+            onChange={updateMeeting} 
+            required/>
         </Form.Group>
+        {/* Nick add your API stuff in the Form.Control below
+        also should probably update the createMeeting function*/}
+        {/* name="" value={} onChange={updateMeeting} required/ */}
+        <FormGroup>
+          <Form.Label>Alumni Description</Form.Label>
+          <Form.Control  
+            as="textarea" 
+            row="5"
+            cols="30"/>
+        </FormGroup>
         <Button variant="primary" type="submit">Submit</Button>
       </Form>
     </Container>
