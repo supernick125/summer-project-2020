b066f1969bf90eabbfb529b52be7df8d5b9ce6a4
gm1234-a1 <41346742+gm1234-a1@users.noreply.github.com>
2021-01-31 15:28:26 -0500

    Create CheckPage.js

---
+++ client/src/pages/Check/CheckPage.js
@@ -0,0 +1,43 @@
+import React, { useState, useContext } from 'react';
+import Axios from 'axios';
+import Title from '../svgs/Title';
+import { Redirect, Link } from 'react-router-dom';
+
+import { Container, Nav, Navbar, Form, FormControl, Col , InputGroup, Button } from 'react-bootstrap';
+import './style.css';
+
+import { Context as AuthContext } from '../../context/Auth';
+
+export default () => {
+
+  const { authUser, setAuthUser } = useContext(AuthContext);
+
+  const logoutUser = (event) => {
+    event.preventDefault();
+    document.cookie = 'x-auth-token= ; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    delete Axios.defaults.headers.common['x-auth-token'];
+    setAuthUser({
+      action: 'LOGOUT_USER',
+      payload: null
+    });
+    window.location.reload();
+  }
+
+  const name = authUser.user.firstname + ' ' + authUser.user.lastname;
+
+  return (
+    <Navbar className="bar" bg="light" expand="lg">
+      <Link to='/'><Title className='svg'/></Link>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Form inline className=" ml-auto mr-sm-2">
+          <p>{name}</p>
+          <i className="fas fa-user-circle fa-2x" id="user-icon"></i>
+          <Button variant="primary" type="submit" id="logout-button" onClick={logoutUser}>
+            Log Out
+          </Button>
+        </Form>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
