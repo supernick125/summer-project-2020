81f4582db095c9ed64a39abbbba86fe3b861fc2b
dag2226 <65051137+dag2226@users.noreply.github.com>
2021-03-05 11:28:37 -0500

    Added page specific id to limit .css

--- client/src/pages/Home/HomePage.js
+++ client/src/pages/Home/HomePage.js
@@ -9,55 +9,57 @@ export default () => {
   return (
     <div className='h-100'>
       <HomeNavbar />
-      <div id="banner">
-        <h1>MENTOR MEETINGS</h1>
-        <div>Stay in touch with your rowing alumni mentors by keeping track of your mentor meetings here.</div>
-      </div>
-      <div id="profile">
-        <img id="profilePicture" src={UserIcon}/>
-        <div id="profileInfo">
-          <h1>Jamie Dimon</h1>
-          <h1>Class of 1978</h1>
+      <div id="homePage">
+        <div id="banner">
+          <h1>MENTOR MEETINGS</h1>
+          <div>Stay in touch with your rowing alumni mentors by keeping track of your mentor meetings here.</div>
         </div>
-        <div id="position">Chief Executive Officer of JP Morgan Chase</div>
-        <div id="connection">Connected based on job interest and hometown.</div>
-      </div>
-      <div id="timeline">
-        <div>Meeting Timeline</div>
-        <div id="scrollmenu">
-          <div class="meetings">
-            <img class="icons" src={EventIcon}/>
-            <div>X months until the next meeting</div>
-            <button class="calendarButtons">Schedule now</button>
+        <div id="profile">
+          <img id="profilePicture" src={UserIcon}/>
+          <div id="profileInfo">
+            <h1>Jamie Dimon</h1>
+            <h1>Class of 1978</h1>
           </div>
-          <div class="meetings">
-            <img class="icons" src={EventIcon}/>
-            <div>Met on X</div>
-            <button class="calendarButtons">Add a short description</button>
-          </div>
-          <div class="meetings">
-            <img class="icons" src={EventIcon}/>
-            <div>Meeting 3</div>
-          </div>
-          <div class="meetings">
-            <img class="icons" src={EventIcon}/>
-            <div>Meeting 4</div>
-          </div>
-          <div class="meetings">
-            <img class="icons" src={EventIcon}/>
-            <div>Meeting 5</div>
-          </div>
-          <div class="meetings">
-            <img class="icons" src={EventIcon}/>
-            <div>Meeting 6</div>
-          </div>
-          <div class="meetings">
-            <img class="icons" src={EventIcon}/>
-            <div>Meeting 7</div>
-          </div>
-          <div class="meetings">
-            <img class="icons" src={EventIcon}/>
-            <div>Meeting 8</div>
+          <div id="position">Chief Executive Officer of JP Morgan Chase</div>
+          <div id="connection">Connected based on job interest and hometown.</div>
+        </div>
+        <div id="timeline">
+          <div>Meeting Timeline</div>
+          <div id="scrollmenu">
+            <div class="meetings">
+              <img class="icons" src={EventIcon}/>
+              <div>X months until the next meeting</div>
+              <button class="calendarButtons">Schedule now</button>
+            </div>
+            <div class="meetings">
+              <img class="icons" src={EventIcon}/>
+              <div>Met on X</div>
+              <button class="calendarButtons">Add a short description</button>
+            </div>
+            <div class="meetings">
+              <img class="icons" src={EventIcon}/>
+              <div>Meeting 3</div>
+            </div>
+            <div class="meetings">
+              <img class="icons" src={EventIcon}/>
+              <div>Meeting 4</div>
+            </div>
+            <div class="meetings">
+              <img class="icons" src={EventIcon}/>
+              <div>Meeting 5</div>
+            </div>
+            <div class="meetings">
+              <img class="icons" src={EventIcon}/>
+              <div>Meeting 6</div>
+            </div>
+            <div class="meetings">
+              <img class="icons" src={EventIcon}/>
+              <div>Meeting 7</div>
+            </div>
+            <div class="meetings">
+              <img class="icons" src={EventIcon}/>
+              <div>Meeting 8</div>
+            </div>
           </div>
         </div>
       </div>
