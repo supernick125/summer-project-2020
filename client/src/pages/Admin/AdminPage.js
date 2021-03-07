48b6404738583217c7b03df54f321897781ce382
gm1234-a1 <41346742+gm1234-a1@users.noreply.github.com>
2021-03-07 15:27:29 -0500

    Update AdminPage.js

--- client/src/pages/Admin/AdminPage.js
+++ client/src/pages/Admin/AdminPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CreateAlumni from '../../components/CreateAlumni/CreateAlumni';
 import CreateMeeting from '../../components/CreateMeeting/CreateMeeting';
+import { Container, Header, Form, Input } from 'semantic-ui-react';
 import './style.css'
 
 const style = {
@@ -20,60 +21,99 @@ const style = {
 };
 
 export default () => {
-  
-  var soupkitchens = ['Zamie', 'Gabe', 'Jonny', 'Daniel', 'Andrei', 'Daniel', 'Nick'];
-  
+
+  var students = ['Zamie', 'Gabe', 'Jonny', 'Daniel', 'Andrei', 'Greg', 'Nick', 'Nathan'];
+
   function updateCurrentSearch(newS){
-    
+
   };
-  
+
   function searchMe(toFind)
   {
     var i = 0;
     var newList = []
-    while(i<soupkitchens.length)
+    while(i<students.length)
     {
-      if(!soupkitchens[i].search(toFind))
+      if(!students[i].search(toFind))
       {
-        newList[newList.length] = soupkitchens[i];
+        newList[newList.length] = students[i];
       }
       i++;
     }
 
-    if (newList.length === soupkitchens.length) {
+    if (newList.length === students.length) {
       return;
     }
 
     return (
-      <ol>
-        {newList.map(variable => (
-          <h2 key={variable}>{variable}</h2>
-        ))}
-      </ol>
+      <div>
+        <ol>
+          {newList.map(variable => (
+            <h2 key={variable}>{variable}</h2>
+          ))}
+        </ol>
+      </div>
     );
   }
 
   return (
     <div>
-      <CreateAlumni />
-      <CreateMeeting />
-      <Form style={style.searchbar}>
-          <Form.Field>
-            <Input
-//              id='search'
-//              type='search'
-//              name='search'
-//              value={currentSearch.search}
-//              size='massive'
-//              onChange={updateCurrentSearch}
-//              onClick={console.log(currentSearch.search)}
-//              icon='search'
-              placeholder='Find a student!'
-            />
-          </Form.Field>
-        </Form>
-      //To search, change the word in quotes. So, for example, change "Zamie" to "Gabe"
-      <h1>{searchMe("Zamie")}</h1>
+      <div class='row'>
+        <div class='column'>
+          <div class='wrap'>
+            <div class="search">
+              <Form style={style.searchbar}>
+                <Form.Field>
+                  <Input
+                   // id='search'
+                   // type='search'
+                   // name='search'
+                   // value={currentSearch.search}
+                   // size='massive'
+                   // onChange={updateCurrentSearch}
+                   // onClick={console.log(currentSearch.search)}
+                   // icon='search'
+                    placeholder='Find a student!'
+                  />
+                </Form.Field>
+              </Form>
+            </div>
+            <div class="top">
+              <div class="hero"></div>
+            </div>
+            <main>
+              {searchMe("G")}
+            </main>
+          </div>
+        </div>
+        <div class='column'>
+          <div class='wrap'>
+            <div class="search">
+              <Form style={style.searchbar}>
+                <Form.Field>
+                  <Input
+                   // id='search'
+                   // type='search'
+                   // name='search'
+                   // value={currentSearch.search}
+                   // size='massive'
+                   // onChange={updateCurrentSearch}
+                   // onClick={console.log(currentSearch.search)}
+                   // icon='search'
+                    placeholder='Find a student!'
+                  />
+                </Form.Field>
+              </Form>
+            </div>
+            <div class="top">
+              <div class="hero"></div>
+            </div>
+            <main>
+              {searchMe("N")}
+            </main>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
